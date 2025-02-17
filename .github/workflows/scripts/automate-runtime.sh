f26c8cbf4aa0750797c3bb12e098d940221698ca
Shreya Laxminarayana Rao <147193825+ShreyaLaxminarayan@users.noreply.github.com>
2025-02-17 13:45:24 +0530

    Update automate-runtime.sh

--- .github/workflows/scripts/automate-runtime.sh
+++ .github/workflows/scripts/automate-runtime.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+set -x
 
+BASEDIR=$(pwd)
+message=()
+version=""
+commit_id=""
+attempts=0
+max_attepts=3
+
+function clone_runtime {
+	git clone --branch ${1:-main} https://github.com/dotnet/runtime
+	pushd runtime
+	commit_id=$(git show HEAD --pretty=format:"%h" --no-patch)
+	message+=("commit id:- ${commit_id} <br>")
+	popd
+}
+
+clone_runtime
